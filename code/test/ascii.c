3b1908652ff5be7903c07588848d5612f435e5e3
vvthai10 <vuvanthai1410@gmail.com>
2022-10-10 21:16:34 +0700

    update ascii

--- code/test/ascii.c
+++ code/test/ascii.c
@@ -1,16 +1,16 @@
 #include "syscall.h"
 
-int
-main()
+int main()
 {
-    char* content = (char*) malloc(4) ;
-    content[0] = '\t';
-    content[2] = '\n';
-    for (int i = 33; i < 127 ; i++){
-        PrintNum(i);        
-        content[1] = (char) i;    
-        PrintString(content);        
-    }    
-    free(content);
+  int i;
+  char* content = (char*) malloc(4) ;
+  content[0] = '\t';
+  content[2] = '\n';
+  for (i = 33; i < 127 ; i++){
+      PrintNum(i);        
+      content[1] = (char) i;    
+      PrintString(content);        
+  }    
+  free(content);
   Halt();  
 }
