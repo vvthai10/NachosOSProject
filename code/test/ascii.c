c6d1e86f7c9b89661a80c211ed4945f4628443c0
vvthai10 <vuvanthai1410@gmail.com>
2022-10-10 21:22:14 +0700

    update print ascii

--- code/test/ascii.c
+++ code/test/ascii.c
@@ -3,14 +3,11 @@
 int main()
 {
   int i;
-  char* content = (char*) malloc(4) ;
-  content[0] = '\t';
-  content[2] = '\n';
-  for (i = 33; i < 127 ; i++){
-      PrintNum(i);        
-      content[1] = (char) i;    
-      PrintString(content);        
-  }    
-  free(content);
+  for (i = 33; i < 127; i++) {
+        PrintNum(i);
+        PrintChar(' ');
+        PrintChar((char)i);
+        PrintChar('\n');
+    }
   Halt();  
 }
