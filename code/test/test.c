b030a6b300bfb88dae8d56587de47e4513525e13
2002Tuong <tuong7b000@gmail.com>
2022-11-18 13:55:54 +0700

    syscall seek

--- code/test/test.c
+++ code/test/test.c
@@ -1,27 +1,29 @@
 #include"syscall.h"
 int main() {
-    int check = CreateFile("index.txt");
+    // int check = CreateFile("index.txt");
     char fileName[] = "index.txt";
     int i;
     int id;
 
-    for(i = 0; i < 20; i++){
-        id = Open(fileName, 0);
-        if( id != -1){
-            PrintString("File ");
-            PrintString(fileName);
-            PrintString(" open successful\n");
+    // for(i = 0; i < 20; i++){
+    //     id = Open(fileName, 0);
+    //     if( id != -1){
+    //         PrintString("File ");
+    //         PrintString(fileName);
+    //         PrintString(" open successful\n");
 
-            PrintString("File descriptor id: ");
-            PrintNum(id);
-            PrintString("\n");
-
-            Close(id);
-        }
-        else{
-            PrintString("Open file failed\n");
-        }
-    }
-
+    //         PrintString("File descriptor id: ");
+    //         PrintNum(id);
+    //         PrintString("\n");
+    //         PrintString("current positon is: ");
+    //         PrintNum(Seek(-1,id));
+    //         PrintString("\n");
+    //         Close(id);
+    //     }
+    //     else{
+    //         PrintString("Open file failed\n");
+    //     }
+    // }
+    Remove("index.txt");
     Halt();
 }
