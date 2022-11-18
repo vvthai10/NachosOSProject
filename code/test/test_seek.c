e657f0b1c823ae74e3330fee0c894ae0a09455a4
2002Tuong <tuong7b000@gmail.com>
2022-11-19 00:07:27 +0700

    temp

---
+++ code/test/test_seek.c
@@ -0,0 +1,20 @@
+#include"syscall.h"
+
+int main() {
+    int id ;
+    id = Open("test.txt",0);
+    if( id != -1){
+            PrintString("File ");
+            PrintString("test.txt");
+            PrintString(" open successful\n");
+
+            PrintString("File descriptor id: ");
+            PrintNum(id);
+            PrintString("\n");
+            PrintString("current positon is: ");
+            PrintNum(Seek(-1,id));
+            PrintString("\n");
+            Remove("test.txt");
+            Close(id);
+        }
+}
