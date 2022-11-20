7ef598b8707a41c326126bb795f3145ae1bea5c9
2002Tuong <tuong7b000@gmail.com>
2022-11-20 22:29:40 +0700

    delete file

--- code/test/shell.c
+++ code/test/shell.c
@@ -4,8 +4,8 @@ int
 main()
 {
     SpaceId newProc;
-    OpenFileId input = ConsoleInput;
-    OpenFileId output = ConsoleOutput;
+    OpenFileId input = 0;
+    OpenFileId output = 1;
     char prompt[2], ch, buffer[60];
     int i;
 
